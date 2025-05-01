74b9c656ff2f9fb54af88954b56d8d19e8bb51f2
Mathias Cerron <mathiascf21@gmail.com>
2025-05-01 16:13:51 -0500

    update files

--- src/Principal.java
+++ src/Principal.java
@@ -1,22 +1,37 @@
+import com.alulacursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
 
-        miPelicula.titulo = "Super Mario Bross";
-        miPelicula.fechaLanzamiento = 2023;
-        miPelicula.duracionMinutos = 92;
+        miPelicula.setTitulo("Super Mario Bross");
+        miPelicula.setFechaLanzamiento(2023);
+        miPelicula.setDuracionMinutos(92);
+        miPelicula.setIncluidoEnPlan(true);
 
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(9.4);
         miPelicula.evalua(5.7);
         miPelicula.evalua(10.5);
+        System.out.println(miPelicula.getTotalDeEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.titulo = "¿que paso ayer?";
-        otraPelicula.fechaLanzamiento = 2009;
-        otraPelicula.duracionMinutos = 100;
 
-        otraPelicula.muestraFichaTecnica();
+
+
+
+
+
+
+
+
+
+
+//        com.alulacursos.screenmatch.modelos.Pelicula otraPelicula = new com.alulacursos.screenmatch.modelos.Pelicula();
+//        otraPelicula.titulo = "¿que paso ayer?";
+//        otraPelicula.fechaLanzamiento = 2009;
+//        otraPelicula.duracionMinutos = 100;
+//
+//        otraPelicula.muestraFichaTecnica();
     }
 }
